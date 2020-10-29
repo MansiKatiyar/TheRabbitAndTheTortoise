c0f222497b83a23f228b33651be0bced7ab51552
Mansi katiyar <mansi.katiyar_cs19@gla.ac.in>
2020-10-29 14:02:50 +0530

    create the constant MILESTONE in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,12 +1,15 @@
 package thread.tortoise;
 
 public class Tortoise extends Thread {
+
+    private static final int MILESTONES = 5;
+
     public Tortoise() {
         super("Tortoise");
     }
 
     @Override
     public void run() {
-
+        for (int)
     }
 }
