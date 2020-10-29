5a39b695d16802df3e1cba427f556e99b6d029c7
Mansi katiyar <mansi.katiyar_cs19@gla.ac.in>
2020-10-29 14:07:31 +0530

    create the for loop in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -5,11 +5,13 @@ public class Tortoise extends Thread {
     private static final int MILESTONES = 5;
 
     public Tortoise() {
-        super("Tortoise");
+      super("Tortoise");
     }
 
     @Override
     public void run() {
-        for (int)
+        for (int index = 0; index < MILESTONES; index++) {
+
+        }
     }
 }
