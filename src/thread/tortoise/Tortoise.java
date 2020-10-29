2a3f703be3f8ace97341c40fa565c142c3fe6483
Mansi katiyar <mansi.katiyar_cs19@gla.ac.in>
2020-10-29 14:19:11 +0530

    In case 3 we use the try and catch block to handle the exception in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -5,13 +5,31 @@ public class Tortoise extends Thread {
     private static final int MILESTONES = 5;
 
     public Tortoise() {
-      super("Tortoise");
+        super("Tortoise");
     }
 
     @Override
     public void run() {
         for (int index = 0; index < MILESTONES; index++) {
             switch (index) {
+                case 0:
+                    System.out.println("The Tortoise has started from the start line!");
+                    break;
+                case 1:
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    break;
+                case 3:
+                    System.out.println("The Tortoise has reached to the Oak Tree");
+                    System.out.println("The Tortoise is leading and hence is going to sleep!");
+                    try {
+                        Thread.sleep(18000L);
+                    } catch (InterruptedException e) {
+                        System.err.println("The Tortoise sleep got interrupted!");
+                    }
+                    break;
+                case 4:
+                    System.out.println("The Tortoise has reached the finish line!");
+                    break;
 
             }
         }
